9632c60086163097c6b336ffde08911e07bbc87a
luckydonald <mac2019.git@luckydonald.de>
2020-06-01 16:24:02 +0200

    [rp_alias] Fixed regex of Fake-Reply.

--- code/rp_alias/fake_reply.py
+++ code/rp_alias/fake_reply.py
@@ -18,7 +18,7 @@ BAR = '┃'
 ELLIPSIS = '…'
 MAX_LEN = 18
 
-REGEX_STR = rf'{BAR}.+{ELLIPSIS}{SPACES[0]}\s*+\n{BAR} .+\n'
+REGEX_STR = rf'{BAR}.+{ELLIPSIS}{SPACES[0]}+\s*\n{BAR} .+\n'
 REGEX = re.compile(REGEX_STR)
 
 
